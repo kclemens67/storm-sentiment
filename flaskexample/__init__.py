f3ef51c01bf5f299cdb05877ee7945bb36c8472e
kclemens67 <kristinaac@gmail.com>
2016-02-07 01:12:01 -0800

    flask update

--- flaskexample/__init__.py
+++ flaskexample/__init__.py
@@ -4,10 +4,5 @@ from flaskexample import views
 
 
 
-@app.errorhandler(500)
-def page_not_found(e):
-    return render_template('bad_output.html'), 500
-
-
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5000)
--- flaskexample/views.py
+++ flaskexample/views.py
@@ -7,6 +7,8 @@ other_pythondir1 = '/home/ubuntu/anaconda2/lib/python2.7/site-packages/'
 for root, dirs, files in os.walk(other_pythondir1):
 	sys.path.append(root)
 
+ # import python packages 
+
 from flask import render_template
 from flaskexample import app
 from sqlalchemy import create_engine
@@ -21,6 +23,9 @@ import numpy as np
 import regex
 import nltk.classify
 from geopy.geocoders import Nominatim, GoogleV3
+import statistics
+from statistics import median, mean
+
 
 @app.route('/')
 @app.route('/index')
@@ -33,11 +38,11 @@ def cesareans_input():
 
 @app.route('/output')
 def cesareans_output():
-  from sqlalchemy import create_engine
-  from sqlalchemy_utils import database_exists, create_database
-  import pandas as pd
-  import psycopg2
-  import numpy as np
+  #from sqlalchemy import create_engine
+  #from sqlalchemy_utils import database_exists, create_database
+  #import pandas as pd
+  #import psycopg2
+  #import numpy as np
 
   def postgres_connect():
     conn_string = "host='localhost' dbname='final_db' user='root'"
@@ -72,89 +77,110 @@ def cesareans_output():
   query_lat = location.latitude
   query_lon = location.longitude
 
-  query = """SELECT date_stamp,location,org_text,temp,weather_rating,norm_response_score, point(%s, %s) <@> point(long, lat)::point AS tweet_distance FROM tweet_weather_table WHERE (point(%s, %s) <@> point(long, lat)) < 10 ORDER by tweet_distance;""" %(query_lon,query_lat,query_lon,query_lat)
+  query = """SELECT date_stamp,location,org_text,temp,weather_rating,num_weather_words,norm_response_score, point(%s, %s) <@> point(long, lat)::point AS tweet_distance 
+    FROM tweet_weather_table 
+    WHERE (point(%s, %s) <@> point(long, lat)) < 10 
+    ORDER by tweet_distance;""" %(query_lon,query_lat,query_lon,query_lat)
 
+    # convert query to pd df
   query_results=pd.read_sql_query(query,con)
 
-  if query_results.empty:
-      print 'oops!'
+    # check to make sure pd df is not empty or not enough tweets
+  if query_results.empty or len(query_results) < 10:
+      print 'not enough tweets!'
       return render_template("bad_output.html")
-  
-  neg_data = query_results.sort_values('norm_response_score',axis = 0, ascending=True)
-  pos_data = query_results.sort_values('norm_response_score',axis = 0, ascending=False)
+    
+    # get the negative and positive tweets and seperate into two lists
+  neg_data = query_results.loc[query_results['norm_response_score'] < 0].sort_values('norm_response_score',axis = 0, ascending=True)
+  pos_data = query_results.loc[query_results['norm_response_score'] > 0].sort_values('norm_response_score',axis = 0, ascending=False)
 
-  if len(neg_data) and len(pos_data) < 10:
-    print 'not enough tweets'
-    return render_template("bad_output.html")
+    # get the response scores for neg and pos and factor in num of weather words
 
-  neg_data = neg_data[:10]
-  pos_data = pos_data[:10]
+  neg_data_scores = neg_data['norm_response_score']*neg_data['num_weather_words']
+  pos_data_scores = pos_data['norm_response_score']*pos_data['num_weather_words']
 
-  neg_response_score = neg_data['norm_response_score']
+    # normalize median values to number of tweets in each and take abs() 
+  adj_median_neg = abs(median(neg_data_scores)*len(neg_data))
+  adj_median_pos = median(pos_data_scores)*len(pos_data)
+
+
+  if adj_median_neg > adj_median_pos:
+      percent_diff = (adj_median_neg-adj_median_pos)/float(adj_median_neg+adj_median_pos)*-1
+      
+  if adj_median_neg < adj_median_pos:
+      percent_diff = (adj_median_pos-adj_median_neg)/float(adj_median_neg+adj_median_pos)
+
+  if adj_median_neg == adj_median_pos:
+      percent_diff = 0
+      
+
+  percent_diff = percent_diff*100
+
+    # get response scores and weather ratings for most negative tweets
   neg_weather_rating = [ x for x in neg_data['weather_rating']]
 
-  pos_response_score = pos_data['norm_response_score']
+    # get response socres and weather ratings for most positive tweets
   pos_weather_rating = [ x for x in pos_data['weather_rating']]
 
-  agg_weather_rating = int(np.sum(neg_weather_rating + pos_weather_rating))/20
+    # calcuate aggregate weather rating 
 
-  print neg_weather_rating, "neg weather rating"
-  print pos_weather_rating, "pos weather rating"
+  agg_weather_rating = int(np.sum(neg_weather_rating + pos_weather_rating))/int((len(neg_weather_rating)+len(pos_weather_rating)))
 
-  print agg_weather_rating, "agg weather rating"
 
-  response_score = [x for x in neg_data['norm_response_score'].apply(int)]+[x for x in pos_data['norm_response_score'].apply(int)]
-
-  #print response_score
-  agg_response_score = int(np.sum(response_score))
-
-  temp = [x for x in neg_data['temp'].apply(int)]+[x for x in pos_data['temp'].apply(int)]
+    # calculate average temp (should be about the same for all since around the same time/location)
+  temp = [x for x in query_results['temp'].apply(int)]
   avg_temp = np.sum(temp)/len(temp)
 
-
+    # get orginal tweet text body for neg and pos lists
   neg_tweets = [ x for x in neg_data['org_text']]
   pos_tweets = [ x for x in pos_data['org_text']]
 
-  
+    # clean up the language in the tweet body
   neg_censored_list = censor_tweets(neg_tweets)
-
-    
   pos_censored_list = censor_tweets(pos_tweets)
-    
 
+    # get the dates for the tweets in neg and pos lists
   neg_dates = [x for x in neg_data['date_stamp']]
   pos_dates = [x for x in pos_data['date_stamp']]
 
+    # representitive tweets 
+  example_tweets_neg = neg_censored_list[:3]
+  example_tweets_pos = pos_censored_list[:3]
 
-  #avg_response_score = np.sum(response_score)/len(response_score)
-  if int(agg_response_score) < 0:
-    example_tweet = neg_censored_list[:5]
-    example_score = neg_response_score
-    print example_tweet
 
-  if int(agg_response_score) > 0: 
-    example_tweet = pos_censored_list[:5]
-    example_score = pos_response_score
-    print example_tweet
-  if int(agg_response_score) == 0: 
-    example_tweet = "This place is pretty neutral about the current weather"
-    example_score = 0
-    descriptiont = "neutral"
- 
-  if 10 > agg_response_score > 1:
-    description = " positive"
 
-  if agg_response_score > 10:
-    description = " very positive"
+  if int(percent_diff) < -75:
+      description = 'extremely negative'
 
-  if -5 < agg_response_score < 0:
-    description = " negative"
+  if -75 <= int(percent_diff) < -50:
+      description = 'very negative'
 
-  if agg_response_score < -5:
-    description = " very negative"
+  if -50 <= int(percent_diff) < -25:
+      description = 'negative'
+
+  if -25 <= int(percent_diff) < 0:
+      description = 'somewhat negative'
+
+  if int(percent_diff) > 75:
+      description = 'extremely positive'
+
+  if 75 >= int(percent_diff) > 50:
+      description = 'very positive'
+
+  if 50 >= int(percent_diff) > 25:
+      description = 'positive'
+
+  if 25 >= int(percent_diff) > 0:
+      description = 'somewhat positive'
+
+  if int(percent_diff) == 0: 
+      description = " neutral"
+
+  print percent_diff
+  print description
+  print example_tweets_neg
+  print example_tweets_pos
 
   the_result = avg_temp
 
-  agg_response_score = ' '
-
-  return render_template("output.html", query_results = the_result,agg_weather_rating=agg_weather_rating, response_score=agg_response_score,example_score=example_score,example_tweet=example_tweet,description=description)
+  return render_template("output.html", query_results = the_result,agg_weather_rating=agg_weather_rating, response_score=percent_diff,example_tweets_pos=example_tweets_pos,example_tweets_neg=example_tweets_neg,description=description)
